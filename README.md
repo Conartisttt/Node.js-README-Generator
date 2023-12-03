7b4da82d516ac55918ea78510ff7200516bc395a
Conartisttt <connerwmar@gmail.com>
2023-12-03 18:06:26 -0500

    badges working, readme generating

---
+++ README.md
@@ -0,0 +1,101 @@
+# Node.js-README-Generator
+
+References:
+[Inquirer package](https://www.npmjs.com/package/inquirer/v/8.2.4). Review the [Professional README Guide](https://coding-boot-camp.github.io/full-stack/github/)
+
+Acceptance Criteria:
+* README is generated with the following items/sections:
+    - title of project
+    - description
+    - table of contents
+    - installation
+    - Usage
+    - License
+    - Contributing
+    - Tests
+
+* Enter project title - displayed as title of readme
+* Choose license from list:
+    - badge is added near the top of the README
+    - notice is added to License section explaining which license app is covered under
+* Enter github username - add to quetions section with link to github profile
+* Enter email address - add to questions section with instrutions on how to reach me with additional questions
+* When i click on the links in the table of contents - taken to corresponding section
+
+* what the app is for
+* how to use it
+* how to install it
+* how to report issues
+* how to make contributions
+
+* application will be invoked by using node index.js
+* provide link to video of walkthrough using screencastify
+* 
+
+
+## Table of Contents
+[Description](#description)
+<br>
+[Technologies Used](#technologies-used)
+<br>
+[Installation](#installation)
+<br>
+[Usage](#usage)
+<br>
+[Contributing](#contributing)
+<br>
+[Tests](#tests)
+<br>
+[License](#license)
+<br>
+[Questions](#questions)
+<br>
+[Credits](#credits)
+
+
+
+
+
+
+## Description
+
+The purpose of the application is to allow developers to dynamically generate a high-quality README file for their own applications using the command-line so that they can devote more time to working on the project.
+
+The following video demonstrates the applications functionality:
+
+![README Generator](insert link to video here)
+
+## Installation
+* Run the application by opening terminal and using command: 
+(make code style) - node index.js
+
+## Usage
+
+## Contributing
+
+## Tests
+
+## License
+
+## Questions
+
+
+## 
+
+## Features
+
+
+
+
+
+## Technologies Used
+
+* JavaScript
+* Node.js
+* Inquirer package
+* fs 
+
+## Credits
+
+* Starter code provided by edX Boot Camps LLC.
+* All updates made by Conner Martin aka Conartisttt
--- index.js
+++ index.js
@@ -1,13 +1,197 @@
 // TODO: Include packages needed for this application
 
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is the name of your file? ex: README.md",
+    "What is the title?",
+    "What is the description?",
+    "What technologies were used to create the application?",
+    "How do users install the application?",
+    "How do users use the application?",
+    "What are the contribution guidelines?",
+    "How can you test the application?",
+    "What license is the application covered under?",
+    "What is your GitHub username?",
+    "What is your email address?",
+    "Add any credits to project:"
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Success!')
+    )
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: questions[0],
+                name: 'file',
+            },
+            {
+                type: 'input',
+                message: questions[1],
+                name: 'title',
+            },
+            {
+                type: 'input',
+                message: questions[2],
+                name: 'description',
+            },
+            {
+                type: 'checkbox',
+                message: questions[3],
+                choices: [" HTML", " JavaScript", " CSS", " Java", " React"],
+                name: 'technologies',
+            },
+            {
+                type: 'input',
+                message: questions[4],
+                name: 'installation',
+            },
+            {
+                type: 'input',
+                message: questions[5],
+                name: 'usage',
+            },
+            {
+                type: 'input',
+                message: questions[6],
+                name: 'contributing',
+            },
+            {
+                type: 'input',
+                message: questions[7],
+                name: 'tests',
+            },
+            {
+                type: 'list',
+                message: questions[8],
+                choices: [
+                    {
+                        name: "MIT",
+                        value: "MIT",
+                    },
+                    {
+                        name: "Apache",
+                        value: "Apache",
+                    },
+                    {
+                        name: "Boost",
+                        value: "Boost",
+                    },
+                    {
+                        name: "Eclipse",
+                        value: "Eclipse",
+                    },
+                ],
+                name: 'license',
+            },
+            {
+                type: 'input',
+                message: questions[9],
+                name: 'github',
+            },
+            {
+                type: 'input',
+                message: questions[10],
+                name: 'email',
+            },
+            {
+                type: 'input',
+                message: questions[11],
+                name: 'credits',
+            },
+        ])
+        .then((response) => {
+            generateData(response);
+        }
+        );
+}
+
+function generateData(response) {
+    console.log(response.license)
+    const file = response.file;
+    const license = response.license;
+    let badgeLink = ``;
+    switch (license) {
+        case 'MIT':
+            badgeLink = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+            break;
+        case 'Apache':
+            badgeLink = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+            break;
+        case 'Boost':
+            badgeLink = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
+            break;
+        case 'Eclipse':
+            badgeLink = '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)'
+            break;
+
+        default:
+            break;
+    }
+    const content = `
+# ${response.title}
+${badgeLink}
+
+## Table of Contents
+[Description](#description)
+<br>
+[Technologies Used](#technologies-used)
+<br>
+[Installation](#installation)
+<br>
+[Usage](#usage)
+<br>
+[Contributing](#contributing)
+<br>
+[Tests](#tests)
+<br>
+[License](#license)
+<br>
+[Questions](#questions)
+<br>
+[Credits](#credits)
+
+## Description
+${response.description}
+
+## Technologies Used
+${response.technologies}
+
+## Installation
+${response.installation}
+
+## Usage
+${response.usage}
+
+## Contributing
+${response.contributing}
+
+## Tests
+${response.tests}
+
+## License
+This project is covered under the ${response.license} license.
+
+## Questions
+For any questions about the application, email me at: ${response.email} or find me on github at: ${response.github}
+
+## Credits
+${response.credits}
+    
+    `;
+    writeToFile(file, content)
+}
 
 // Function call to initialize app
 init();
